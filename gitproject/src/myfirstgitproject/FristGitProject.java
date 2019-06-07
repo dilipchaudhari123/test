5c7878a014fedf11986d63038202eaf746026a9b
Karan'sd PC <Karan'sd PC@192.168.5.10>
2019-06-07 20:31:05 +0530

    my second change

--- gitproject/src/myfirstgitproject/FristGitProject.java
+++ gitproject/src/myfirstgitproject/FristGitProject.java
@@ -5,6 +5,8 @@ public class FristGitProject {
 	public static void main(String[] args) 
 	{
 		System.out.println("hello world");
+		System.out.println("my name is dilip");
+		System.out.println("hi world");
 	}
 
 }
